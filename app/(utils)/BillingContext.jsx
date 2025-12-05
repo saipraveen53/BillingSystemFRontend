0b14e52b959629033f4ce1c412e450ed315ddbb7
saipraveen53 <145278161+saipraveen53@users.noreply.github.com>
2025-12-05 10:34:10 +0530

    Added get Endpoint For Admins:

--- app/(utils)/BillingContext.jsx
+++ app/(utils)/BillingContext.jsx
@@ -5,8 +5,9 @@ export const BillContext = createContext();
 const BillingContext = ({children}) => {
     const [isAuthenticated,setAuthenticated]= useState(false);
     const [decoded,setDecoded]= useState(null);
+    const [role,setRole]=useState("");
   return (
-    <BillContext.Provider  value={{isAuthenticated,setAuthenticated,decoded,setDecoded}}  >
+    <BillContext.Provider  value={{isAuthenticated,setAuthenticated,decoded,setDecoded,role,setRole}}  >
         {children}
     </BillContext.Provider>
   )
